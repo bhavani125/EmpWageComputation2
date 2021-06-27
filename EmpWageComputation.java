8c7e99a96540e544d60d34c6ad91ee6ea05d4fe0
bhavani125 <girinenibhavani12@gmil.com>
2021-06-27 23:34:44 +0530

    Added: welcome message

---
+++ EmpWageComputation.java
@@ -0,0 +1,7 @@
+package com.emp;
+
+public class EmpWageComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee  wage computation problem ");
+    }
+}
